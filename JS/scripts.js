f5f1357631d7fdf7e5e867e73d6633e8cd1fac0f
Cristian Lucero and Fausto Guillen <fausto@>
2018-10-15 14:57:47 -0700

    Cipher exercise

---
+++ JS/scripts.js
@@ -0,0 +1,45 @@
+var str = prompt("Enter sentence: ");
+
+var str2 = function (str) {
+    var first = str.charAt(0);
+    var last = str.charAt(str.length-1);
+    return (last + first).toUpperCase();
+};
+
+var str3 = function (str) {
+    var divide = str.length/2;
+    var letter = str.charAt(divide);
+    return letter;
+};
+
+var newName = str3(str) + str + str2(str);
+
+var str4 = str3(str) + str + str2(str);
+str4 = str4.split("");
+str4 = str4.reverse();
+str4 = str4.join("");
+
+alert(str4);
+
+
+
+
+
+
+
+
+
+ //var reverse = function () {
+     //console.log(sentence3().toUpperCase(),sentence2().toUpperCase());//
+
+//alert(sentence);//
+
+//ar firstChar = sentence.charAt(0);//
+//var lastChar = sentence.charAt(sentence.length-1);//
+
+//var reverse = alert(firstChar + lastChar);//
+
+//var reverse2 = function () {
+    //return reverse.split("").reverse().join("");//
+
+//alert(reverse2);//
